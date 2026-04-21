3c6023437cb748a5518331b8b52565c84149df4f
raziq <raziqsufian2002@gmail.com>
2026-04-21 00:49:53 -0400

    Added emotion detection script and test output

---
+++ emotion_detection.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json=input_json, headers=headers)
+
+    return response.text
